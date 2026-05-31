e41637f33c4ce1bd66ad5313ada2d879846893bf
Ayelén <ayelen97aguero@gmail.com>
2026-05-31 03:29:19 -0300

    cambios en index.html y script.js

--- script.js
+++ script.js
@@ -12,9 +12,11 @@ async function fetchData() {
 
         const data = await response.json();
         users = data.data;
+        console.log(users);
         renderTable();
         renderFrecuenciaNiveles();
         renderFrecuenciaCursos();
+        renderEstadisticos();
     } catch (error) {
         console.error(error);
     }
@@ -31,7 +33,7 @@ function renderTable() {
         row.innerHTML = `
         <td>${user.nombre}</td>
         <td>${user.apellido}</td>
-        <td>${user.edad}</td>
+        <td>${user.Edad}</td>
         <td>${user.curso}</td>
         <td>${user.nivel}</td>
         `;
@@ -129,6 +131,75 @@ function renderFrecuenciaCursos() {
 
     }
 
+}
+
+function obtenerEdades() {
+    return users.map(user => 
+        user.Edad);
+}
+function renderEstadisticos() {
+    let edades = obtenerEdades();
+//Media
+let suma = 0;
+edades.forEach(edad => {
+    suma += edad;
+});
+let media = suma / edades.length;
+
+//Maximo 
+let max = edades[0];
+edades.forEach(edad => {
+    if (edad > max) {
+        max = edad; 
+    }
+});
+
+//Minimo
+let min = edades[0];
+edades.forEach(edad => {
+    if (edad < min) {
+        min = edad; 
+    }
+});
+
+// Mediana
+function calcularMediana(arr) {
+    let ordenado = [...arr].sort((a, b) => a - b);
+    let mitad = Math.floor(ordenado.length / 2);
+    if (ordenado.length % 2 === 0) {
+        return (ordenado[mitad - 1] + ordenado[mitad]) / 2;
+    } else {
+        return ordenado[mitad];
+    }
+}
+
+
+let edadesOrdenadas = [...edades].sort((a, b) => a - b);
+let mediana = calcularMediana(edadesOrdenadas);
+
+// Cuartiles
+let mitad = Math.floor(edadesOrdenadas.length / 2);
+let primeraMitad = edadesOrdenadas.slice(0, mitad);
+let segundaMitad = edadesOrdenadas.slice(mitad);
+let q1 = calcularMediana(primeraMitad);
+let q2 = calcularMediana(segundaMitad);
+
+//Desviacion Estandar
+let sumaDiferencias = 0;
+edades.forEach(edad => {
+    sumaDiferencias += Math.pow(edad - media, 2);
+});
+let promedioDiferencias = sumaDiferencias / edades.length;
+let desvio = Math.sqrt(promedioDiferencias);
+
+
+document.getElementById("mediaValor").textContent = media;
+document.getElementById("medianaValor").textContent = mediana;
+document.getElementById("maximoValor").textContent = max;
+document.getElementById("minimoValor").textContent = min;
+document.getElementById("q1Valor").textContent = q1;
+document.getElementById("q2Valor").textContent = q2;
+document.getElementById("desvioEstandarValor").textContent = desvio;
 }
 
 fetchData();
